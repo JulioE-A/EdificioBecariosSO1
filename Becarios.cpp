f61f823231d6c925ce72fe0ad37c3298506745e3
Ariel Armando Gómez Ramos <arielgmez@unah.hn>
2021-04-22 21:21:24 -0600

    Update Becarios.cpp

--- Becarios.cpp
+++ Becarios.cpp
@@ -12,6 +12,7 @@ sem_t tareas;
 sem_t imprimir;
 int tareasTerminadas = 0;
 int tareasTotales = 10;
+int listaTareas[20][2];
 bool entorpecedor = false;
 
 void resolverTarea(){
@@ -31,12 +32,15 @@ void *becario(void *dato){
 	int *id;
 
 	id=(int *)dato;
-	
+	int horasTrabajadas = 0;
 	while(tareasTerminadas<tareasTotales){
+		int tareaActual = tareasTerminadas;
 		tareasTerminadas++;
 		bool pegado = false;
 		int molestado = 0;
+		
 		sem_wait(&tareas);
+		
 		if(!entorpecedor){
 			entorpecedor = true;
 			pegado = true;
@@ -44,23 +48,23 @@ void *becario(void *dato){
 			cout<<"Entorpecedor pegado a becario "<<*id<<endl;
 			sem_post(&imprimir);	
 		}
+		
 		sem_wait(&imprimir);
 		cout<<"Becario "<<*id<<" escogiendo tarea"<<endl;
 		resolverTarea();
 		sem_post(&imprimir);
+		
 		sem_post(&tareas);	
 		
-		srand(time(NULL));
-		int random = rand()%(5-1+1)+1;
 		
 		//resolviendo problema
-		for(int i=0; i<random;i++){
+		for(int i=0; i<listaTareas[tareaActual][1];i++){
 			resolverTarea();
 			if(pegado){
 				resolverTarea();
 				molestado++;
 			}
-
+			horasTrabajadas++;
 		}
 		//resolviendo problema
 		
@@ -69,19 +73,32 @@ void *becario(void *dato){
 		}
 		
 		sem_wait(&imprimir);
-		cout<<"Becario: "<<*id<<" resolvio tarea "<<endl;
+		cout<<"Becario "<<*id<<" resolvio tarea "<<endl;
 		sem_post(&imprimir);
 		
 	}
+	sem_wait(&imprimir);
+	cout<<"Becario: "<<*id<<" horas trabajadas: "<<horasTrabajadas<<endl;
+	sem_post(&imprimir);
 }
 
 
 int main(int argc, char *argv[])
 {
-	int status;
-	int ids[10]={1,2,3,4,5,6,7,8,9,10};
+    int status;
+    int ids[5]={1,2,3,4,5};
     sem_init(&tareas,0, 1);
     sem_init(&imprimir,0,1);
+    srand(time(NULL));
+    
+    for (int i =0; i<20;i++){
+		
+		int random = rand()%(3-1+1)+1;
+		listaTareas[i][0]=i;
+		listaTareas[i][1]=random;
+		cout<<random<<endl;
+	}
+    
     // creating thread objects
     pthread_t thrd_1;
     pthread_t thrd_2;
@@ -96,18 +113,18 @@ int main(int argc, char *argv[])
     
     pthread_create(&thrd_2,NULL,&becario,&ids[1]);
     
-	pthread_create(&thrd_3,NULL,&becario,&ids[2]);
+    pthread_create(&thrd_3,NULL,&becario,&ids[2]);
 	
-	pthread_create(&thrd_4,NULL,&becario,&ids[3]);
+    pthread_create(&thrd_4,NULL,&becario,&ids[3]);
 	   
     pthread_create(&thrd_5,NULL,&becario,&ids[4]);
-    
-	pthread_create(&thrd_6,NULL,&becario,&ids[5]);
+	
+    pthread_create(&thrd_6,NULL,&becario,&ids[5]);
     
     
 
     pthread_join(thrd_1, (void **)&status);
-	pthread_join(thrd_2, (void **)&status);
+    pthread_join(thrd_2, (void **)&status);
     pthread_join(thrd_3, (void **)&status);
     pthread_join(thrd_4, (void **)&status);
     pthread_join(thrd_5, (void **)&status);
