dc2e386fa20836e1456fc9a67107b9f95418e62a
Ariel Armando Gómez Ramos <arielgmez@unah.hn>
2021-04-22 22:32:46 -0600

    Update Becarios.cpp

--- Becarios.cpp
+++ Becarios.cpp
@@ -13,6 +13,7 @@ sem_t imprimir;
 int tareasTerminadas = 0;
 int listaTareas[20][2];
 bool entorpecedor = false;
+int horasBecarios[5];
 
 void resolverTarea(){
 
@@ -20,6 +21,18 @@ void resolverTarea(){
 
     for(i = 0; i < 20000; i++){
         for(j = 0; j < 10000; j++){
+        ;
+	    }
+    }
+
+}
+
+void escogerTarea(){
+
+    int i, j;
+
+    for(i = 0; i < 10000; i++){
+        for(j = 0; j < 5000; j++){
         ;
 	    }
     }
@@ -34,7 +47,7 @@ void *becario(void *dato){
 	int horasTrabajadas = 0;
 	bool abandona = false;
 	
-	while((horasTrabajadas+listaTareas[tareasTerminadas][1])<8){
+	while((horasTrabajadas+listaTareas[tareasTerminadas][1])<9){
 		int tareaActual = tareasTerminadas;
 		tareasTerminadas++;
 		bool pegado = false;
@@ -54,7 +67,7 @@ void *becario(void *dato){
 		
 		sem_wait(&imprimir);
 		cout<<"Becario "<<*id<<" escogiendo tarea"<<endl;
-		resolverTarea();
+		escogerTarea();
 		sem_post(&imprimir);
 		
 		sem_post(&tareas);	
@@ -64,12 +77,18 @@ void *becario(void *dato){
 		
 		for(int i=0; i<listaTareas[tareaActual][1];i++){
 			if(!abandona){
+				//si sobrepasa el limite de veces molestado abandona
 				if(molestado<2){
-				
+					//si el entorpecedor esta pegado se tarda el doble
 					if(pegado){
 						resolverTarea();
 						molestado++;
 						horasTrabajadas++;
+						if(horasTrabajadas>=8){
+							//se acaba jornada, entorpecedor abandona
+							pegado = false;
+							entorpecedor = false;
+						}
 					}
 					resolverTarea();
 					horasTrabajadas++;	
@@ -97,25 +116,27 @@ void *becario(void *dato){
 		sem_post(&imprimir);
 		
 	}
+	
+	horasBecarios[*id] = horasTrabajadas;
 	sem_wait(&imprimir);
-	cout<<"Becario: "<<*id<<" horas trabajadas: "<<horasTrabajadas<<endl;
+	cout<<"Becario "<<*id<<" parcheando"<<endl;
 	sem_post(&imprimir);
 }
 
 
 int main(int argc, char *argv[])
 {
-	int status;
-	int ids[5]={1,2,3,4,5};
+    int status;
+    int ids[5]={1,2,3,4,5};
     sem_init(&tareas,0, 1);
     sem_init(&imprimir,0,1);
     srand(time(NULL));
     
     for (int i =0; i<20;i++){
 		
-		int random = rand()%(3-1+1)+1;
-		listaTareas[i][0]=i;
-		listaTareas[i][1]=random;
+	int random = rand()%(3-1+1)+1;
+	listaTareas[i][0]=i;
+	listaTareas[i][1]=random;
 	}
     
     // creating thread objects
@@ -132,24 +153,27 @@ int main(int argc, char *argv[])
     
     pthread_create(&thrd_2,NULL,&becario,&ids[1]);
     
-	pthread_create(&thrd_3,NULL,&becario,&ids[2]);
+    pthread_create(&thrd_3,NULL,&becario,&ids[2]);
 	
-	pthread_create(&thrd_4,NULL,&becario,&ids[3]);
+    pthread_create(&thrd_4,NULL,&becario,&ids[3]);
 	   
     pthread_create(&thrd_5,NULL,&becario,&ids[4]);
     
-	pthread_create(&thrd_6,NULL,&becario,&ids[5]);
+    pthread_create(&thrd_6,NULL,&becario,&ids[5]);
     
     
 
     pthread_join(thrd_1, (void **)&status);
-	pthread_join(thrd_2, (void **)&status);
+    pthread_join(thrd_2, (void **)&status);
     pthread_join(thrd_3, (void **)&status);
     pthread_join(thrd_4, (void **)&status);
     pthread_join(thrd_5, (void **)&status);
     pthread_join(thrd_6, (void **)&status);
 
-
+	for(int i =0; i<6;i++){
+		cout<<"Becario "<<i<<" horas trabajadas: "<<horasBecarios[i]<<endl;
+		
+	}
 
     system("PAUSE");
     return 0;
